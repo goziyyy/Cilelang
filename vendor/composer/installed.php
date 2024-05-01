1e6a4d950e7bd01a0d908af6e1eb324ff31dcc9a
Gozi <fariscraft709@gmail.com>
2024-05-02 06:58:26 +0700

    Last Fix

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -5,7 +5,7 @@
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => '60c00d06cb74c94a1656c8c998a3987b7450a7e9',
+        'reference' => '86e576d0645a93a9b88f63fbf575487f7ba6b216',
         'name' => 'laravel/laravel',
         'dev' => true,
     ),
@@ -400,7 +400,7 @@
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => '60c00d06cb74c94a1656c8c998a3987b7450a7e9',
+            'reference' => '86e576d0645a93a9b88f63fbf575487f7ba6b216',
             'dev_requirement' => false,
         ),
         'laravel/sail' => array(
@@ -730,8 +730,8 @@
         'psr/log-implementation' => array(
             'dev_requirement' => false,
             'provided' => array(
-                0 => '1.0|2.0',
-                1 => '1.0.0 || 2.0.0 || 3.0.0',
+                0 => '1.0.0 || 2.0.0 || 3.0.0',
+                1 => '1.0|2.0',
             ),
         ),
         'psr/simple-cache' => array(
